f8f8b15d82258026cfb307e007a63fd003d09826
surya <suryasarisa99@gmail.com>
2023-09-18 00:23:48 +0530

    fixed switch-bar pos

--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect, useState, useRef } from "react";
 import { DataContext } from "../context/DataContext";
 import _throttle from "lodash/throttle";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
+import { createPortal } from "react-dom";
+import { AiFillCloseCircle } from "react-icons/ai";
 export default function ProfileView({
   name,
   // data: postsData[name][data_type],
@@ -13,10 +14,14 @@ export default function ProfileView({
 }) {
   const { setPostsData, carouselsLoaded, dispatchLoaded, postsData } =
     useContext(DataContext);
+  const navigate = useNavigate();
   const [loaded, setLoaded] = useState(carouselsLoaded.profile?.[name] || 0);
   const [isLoading, setIsLoading] = useState(true);
   // const [alreadyLoading, setAlreadyLoading] = useState(true);
   const alreadyLoading = useRef(true);
+  const [longPress, setLongPress] = useState(false);
+  const [bigImg, setBigImg] = useState();
+  const dblClick = useRef(null);
 
   useEffect(() => {
     if (loaded == 0) {
@@ -26,6 +31,31 @@ export default function ProfileView({
       });
     } else setTimeout(() => (alreadyLoading.current = false), 400);
   }, [postsData]);
+
+  function showOverlay() {
+    document.getElementById("overlay").classList.remove("hidden");
+    let root = document.getElementById("root");
+    root.style.filter = "blur(15px)";
+    // root.style.maxHeight = "100vh";
+    // root.style.overflow = "hidden";
+  }
+  function closeOverlay() {
+    setLongPress(false);
+
+    document.getElementById("overlay").classList.add("hidden");
+    let root = document.getElementById("root");
+    root.style = "";
+    // root.style.maxHeight = "initial";
+    // root.style.overflow = "initial";
+  }
+
+  useEffect(() => {
+    const closeOverlayOnEscap = (e) => {
+      console.log(e);
+      if (e.key == "Escape") closeOverlay();
+    };
+    document.addEventListener("keydown", closeOverlayOnEscap);
+  }, []);
 
   useEffect(() => {
     window.onscroll = _throttle((e) => {
@@ -49,11 +79,28 @@ export default function ProfileView({
       }
     }, 10);
   }, [postsData, loaded]);
+  const timerRef = useRef(null);
 
   return (
     <div className="profile-view">
       {postsData?.[name]?.[data_type]?.slice(0, loaded)?.map((item, index) => (
-        <Link key={item._id} to={`/profile/${name}/${data_type}/${index}`}>
+        <div
+          key={item._id}
+          onClick={(e) => {
+            setTimeout(() => {
+              if (!dblClick.current) {
+                navigate(`/profile/${name}/${data_type}/${index}`);
+              }
+            }, 200);
+          }}
+          onDoubleClick={(e) => {
+            dblClick.current = true;
+            setTimeout(() => (dblClick.current = false), 300);
+            setLongPress(true);
+            showOverlay();
+            setBigImg(item);
+          }}
+        >
           <div className={"img-box " + data_type}>
             <img
               src={item.images[0]}
@@ -61,8 +108,27 @@ export default function ProfileView({
               alt={`Image ${index}`}
             />
           </div>
-        </Link>
+        </div>
       ))}
+      {longPress &&
+        createPortal(
+          <div>
+            <div className="simple-carousel">
+              {bigImg.images.map((img) => {
+                return (
+                  <div className="img-box" key={img}>
+                    <img src={img} />
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="cross-btn" onClick={closeOverlay}>
+              <AiFillCloseCircle className="cross-icon" />
+            </div>
+          </div>,
+          document.getElementById("overlay")
+        )}
 
       {isLoading && (
         <div className="loader">
